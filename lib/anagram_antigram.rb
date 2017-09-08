62f3335b0a6c8e3cfea985f9af7dc65486253662
Dan Kiss <flowfast47@gmail.com>
2017-09-08 16:22:18 -0700

    add way to test for palindromes regardless of case of word

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -5,10 +5,11 @@ class Anagram
   single_word_at_1
   letters_of_word_at_0
   letters_of_word_at_1
-  first_letter_of_word
 
-  user_input = "Ruby bury"
+
+  user_input = "Level level"
   def anagram_calc(user_input)
+    user_input = user_input.downcase()
     all_words = user_input.split(" ")
     total_number_of_words = all_words.length
     number_of_words = all_words.length
@@ -28,12 +29,13 @@ class Anagram
     x=0
     count_match = 0
     while (x<total_letters_0)
-      first_letter_of_word = letters_of_word_at_0[x]
+      single_letter_of_word = letters_of_word_at_0[x]
         # note first letter is an r
       letters_of_word_at_1.each do |letter_of_word_at_1|
-        if (letter_of_word_at_1 == first_letter_of_word)
+        if (letter_of_word_at_1 == single_letter_of_word)
           puts "good bitch"
           count_match=count_match+1
+          break
           # puts count_match
         else
           puts "no match"
@@ -43,11 +45,21 @@ class Anagram
       # puts x
       x=x+1
     end
-    if count_match == total_letters_0
+    if count_match == total_letters_1
       return "These words are anagrams"
     end
   end
-  anagram_calc(user_input)
+
+  user_input = "Level"
+  def palin_calc(user_input)
+    user_input = user_input.downcase()
+    the_reverse = user_input.reverse()
+    if user_input == the_reverse
+      return "palindrom occured"
+    else
+      "nope"
+    end
+  end
 
 
 
