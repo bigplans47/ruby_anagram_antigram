9bf36b2b8d384f2b2cf3a3205f9c105637a45958
Dan Kiss <flowfast47@gmail.com>
2017-09-08 12:54:07 -0700

    add way to see if first letter appears at all in the second word

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,13 +1,41 @@
 class Anagram
   # puts enter a sentence
   user_input = "ruby bury"
-  def anagram_calc(user_input)
-    words = user_input.split(" ")
-    if words[0].length === words[1].length
-      single_word = words[0]
-      letters_of_word = single_word.split("")
-      return letters_of_word[0]
-    end
+  single_word_at_0
+  single_word_at_1
+  letters_of_word_at_0
+  letters_of_word_at_1
+  first_letter_of_word
 
+  def anagram_calc(user_input)
+    all_words = user_input.split(" ")
+    number_of_words = all_words.length
+
+    # if all_words[0].length === all_words[1].length
+      # assuming working off 2 words only for now
+    single_word_at_0 = all_words[0]
+    single_word_at_1 = all_words[1]
+    letters_of_word_at_0 = single_word_at_0.split("")
+    letters_of_word_at_1 = single_word_at_1.split("")
+    first_letter_of_word = letters_of_word_at_0[0]
+      # note first letter is an r
+    count =0
+    # def find_the_letter(letters_of_word_at_1)
+    #
+    letters_of_word_at_1.each do |letter_of_word_at_1|
+      if (letter_of_word_at_1 == first_letter_of_word)
+        puts "good bitch"
+        count = count + 1
+        if count == 1
+          puts "match occured"
+        end
+      else
+        count = count
+      end
+    end
+    puts count
   end
+
+
+
 end
---
+++ spec/anagram_antigram_spec.rb
@@ -0,0 +1,6 @@
+require('rspec')
+require('pry')
+require('anagram_antigram')
+
+describe('anagram_calc') do
+  it("")
