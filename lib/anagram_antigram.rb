19af2436392b7fdc4634ebab3d2058981b1fb665
Dan Kiss <flowfast47@gmail.com>
2017-09-09 17:08:25 -0700

    can test for 3 and more word anagrams

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -7,54 +7,64 @@ class Anagram
   single_word_at_1 =[]
   letters_of_word_at_0 =[]
   letters_of_word_at_1 =[]
+  user_input = ""
 
-
-
-  # user_input = "Ira air"
+ # user_input.downcase.gsub!(/[^a-z0-9\s]/i, '')
+  # user_input = "Ira air, ari"
+  # user_input = "post, Pots, spot, stop, tops"
   def anagram_calc(user_input)
-    user_input.downcase!()
+    user_input = user_input.downcase.gsub(/[^a-z0-9\s]/i, '')
     all_words = user_input.split(" ")
     number_of_words = all_words.length
-
-    # n = 0
-    # number_of_words-1.times() do
-
-      # if all_words[0].length === all_words[1].length
-        # assuming working off 2 words only for now
     single_word_at_0 = all_words[0]
-    single_word_at_1 = all_words[1]
     letters_of_word_at_0 = single_word_at_0.split("")
     total_letters_0 = letters_of_word_at_0.length
-    letters_of_word_at_1 = single_word_at_1.split("")
-    total_letters_1 = letters_of_word_at_1.length
 
-    x=0
-    count_match = 0
-    while (x<total_letters_0)
-      single_letter_of_word = letters_of_word_at_0[x]
-        # note first letter is an r
-      letters_of_word_at_1.each do |letter_of_word_at_1|
-        if (letter_of_word_at_1 == single_letter_of_word)
-          puts "good"
-          count_match=count_match+1
-          break
-          # puts count_match
-        else
-          puts "no match"
+    y=0
+    count_of_words=0
+    count_of_words_anti=0
+    while (y<all_words.length-1)
+      single_word_at_1 = all_words[y+1]
+      letters_of_word_at_1 = single_word_at_1.split("")
+      total_letters_1 = letters_of_word_at_1.length
+      x=0
+      count_match = 0
+      while (x<total_letters_0)
+        single_letter_of_word = letters_of_word_at_0[x]
+         # note first letter is an r
+        letters_of_word_at_1.each do |letter_of_word_at_1|
+          if (letter_of_word_at_1 == single_letter_of_word)
+            puts "good"
+            count_match=count_match+1
+            break
+           # puts count_match
+          else
+            puts "no match"
+          end
+          puts count_match
         end
-        puts count_match
+        x=x+1
       end
-      # puts x
-      x=x+1
+      y+=1
+      if count_match = total_letters_0
+      count_of_words +=1
+      end
     end
     puts "end of it all"
-    if count_match == total_letters_1
-      return "These words are anagrams"
+    if count_of_words+1 == all_words.length
+      return "all the words are anagrams"
+     # count_of_words +=1
     elsif count_match == 0
-      return "These words are antigrams"
+      puts "possible antigrams"
+      count_of_words_anti +=1
     end
+   # # elsif count_of_words_anti == number_of_words
+   # #   antigram
+   # #   angram count match up with all_words.length
+   # #   puts all are anagram_calc
+   # end
   end
-  # anagram_calc(user_input)
+  anagram_calc(user_input)
 
     # user_input = "Level level"
   def palin_calc(user_input)
@@ -68,7 +78,7 @@ class Anagram
   end
   palin_calc(user_input)
 
-  # user_input = "rib ths boss" 
+  # user_input = "rib ths boss"
   def real_word(user_input)
     user_input = user_input.downcase()
     all_words = user_input.split(" ")
